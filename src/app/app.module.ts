b916ef0630094f69c0edd0a587ee8a7db9da0eea
knastomasz <tomeknas@o2.pl>
2019-04-14 19:32:49 +0200

    Creating 3 components, added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MapComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
